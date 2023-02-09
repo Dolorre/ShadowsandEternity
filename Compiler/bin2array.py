b0436e3300259f3a31672ce4cfe74f65986ec5f1
Dolorre <75703911+Dolorre@users.noreply.github.com>
2023-02-09 16:42:28 -0600

    Add files via upload

---
+++ Compiler/bin2array.py
@@ -0,0 +1,25 @@
+# Original by rainnw. Edited/updated by Dolorre.
+
+# convert BIN GTI files to C++ arrays for Arduino
+
+import sys
+
+# Source file
+input = "sae.bin"
+
+file = open(input,"r")
+ 
+print ("const unsigned char gti[] PROGMEM = { ")
+
+col = 0
+
+while 1==1:
+   data = file.read(1)
+   if data == "": break
+   sys.stdout.write(str(ord(data))+",")
+   col = col + 1
+   if col == 90000: 
+        sys.stdout.write("\n")
+        col = 0
+print ("};")
+
